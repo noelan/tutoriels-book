8046f117a2cf0836d834a7dbcbf7197874d6e445
Noel An <noel.an@hotmail.fr>
2019-12-29 02:08:15 +0100

    Encodage du mot de passe leur de la creation d'utilisateur

---
+++ src/Events/EncodePassword.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class EncodePassword implements EventSubscriberInterface
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['encodePassword', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function encodePassword(ViewEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($user instanceof User && $method == "POST") {
+            $encodedPassword = $this->encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encodedPassword);
+        }
+    }
+}
