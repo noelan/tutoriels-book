7fcbb008757598adfc55bee2f8dc3b03fd58d4af
Noel An <noel.an@hotmail.fr>
2020-09-18 16:37:10 +0200

    please

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,29 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Comment;
-use App\Entity\Post;
-use Faker\Factory;
-use App\Entity\User;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-
-    private $encoder;
-
-    public function __construct(UserPasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
-
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('fr_FR');
-
         // $product = new Product();
         // $manager->persist($product);
+
+        $manager->flush();
     }
 }
